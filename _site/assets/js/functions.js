57c1b10b55704f2cad43adb5b48fb5a22eab3da2
giti <nonotmeplx@gmail.com>
2018-02-27 23:51:28 -0500

    Updating

---
+++ _site/assets/js/functions.js
@@ -0,0 +1,7 @@
+$('.toggled-nav-wrapper').on('click', function(e){
+    e.stopPropagation();
+  });
+
+$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-side-overlay').on('click', function(){
+  $('html').toggleClass('nav-open');
+});
